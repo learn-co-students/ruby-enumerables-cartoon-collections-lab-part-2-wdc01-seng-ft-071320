8cc70c28d9e31946b839d93571005ff72e9322cd
Jina Strickland <jina30@gmail.com>
2020-07-03 02:17:43 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,9 +29,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+  planeteer_calls.select { |calls| valid_calls.include?(calls) }[0]
+end
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
-  planeteer_calls.include?(valid_calls) do | element |
-    "#{element}!"
-  end
-end
