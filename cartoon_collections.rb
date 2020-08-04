58f14c85596037d2d4745b6785759fea4c75e681
alexstephane <alexngundji@gmail.comm.com>
2020-08-04 11:49:30 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
 
 def square_array(array)
+ 
   array.map do |element|
     element * element
   end
@@ -9,10 +10,12 @@ end
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
+
+  planeteer_calls.map do |element1|
+    element1.capitalize+"!"
   
-  planeteer_calls.collect do |call| 
-    call.capitalize + "!"
   end
+  
   
   
 end
@@ -20,14 +23,20 @@ end
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
-  
-    answer = false
+
+  answer = false
   planeteer_calls.each do |call|
     if call.length > 4
       answer = true
     end
   end
     answer
+  
+  
+    
+ 
+    
+    
 end
 
 
@@ -36,16 +45,10 @@ def find_valid_calls(planeteer_calls)
 valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
    # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
 #   # Return the first valid call found, or return nil if no valid calls are found
-  planeteer_calls.find do |planeteer_calls|
-    valid_calls.include?(planeteer_calls)
+  
+planeteer_calls.find do |planeteer_calls|
+  valid_calls.include?(planeteer_calls)
+end
  
  end
- end
 
-# def find_the_cheese(cheese)# code an argument here
-#   # the array below is here to help
-#   cheese_types = ["cheddar", "gouda", "camembert"]
-#   cheese.find do |cheese|
-#     cheese_types.include?(cheese)
-#   end
-# end
